5370dc87329e5f6c083e7376ab877a0a792ac383
AshmitaChimo <asmita.chimoria@varosatech.com>
2025-04-22 21:42:48 +0545

    user login routes

---
+++ config/middleware.js
@@ -0,0 +1,101 @@
+const passport= require('passport');
+const {Strategy: LocalStrategy} = require('passport-local');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const User = require('../models/user'); 
+require ('dotenv').config();
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+    });
+
+passport.deserializeUser((id,done)=>{
+    User.findByPk(id,(err,user)=>{
+        done(err,user);
+    });
+    });
+
+
+passport.use(new LocalStrategy(
+    { usernameField: 'email' },
+    async (email, password, done) => {
+        try {
+            if (!email || email == null) {
+                return done(null, false, { message: 'Email is required' });
+            }
+
+            var user = await User.findOne({ where: { email } });
+            if (!user) {
+                return done(null, false, { message: 'User not found' });
+            }
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if (err) {
+                    return done(err);
+                }
+                if (!isMatch) {
+                    return done(null, false, { message: 'Invalid password' });
+                }
+                return done(null, user);
+            });
+        } catch (error) {
+            console.error('Error during authentication:', error);
+            return done(error);
+        }
+    }
+
+))
+
+
+passport.use('jwt', new JwtStrategy({
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET,
+},
+    async (jwtPayload, done)=>{
+        try {
+            const user = await User.findOne({
+                where:{
+                    id: jwtPayload.userId.id,
+                }
+            });
+            if(!user){
+                return done(null, false, { message: 'User not found' });
+            }
+            return done(null, user);    
+
+        } catch (error) {
+            console.error('Error during JWT authentication:', error);
+            return done(error, false);
+            
+        }
+
+    }));
+
+    function authenticateJWT(req, res, next) {
+        passport.authenticate('jwt', { session: false }, (err, user, info) => {
+            if (info && (info.message.includes('invalid') || info.message.includes('expired'))) {
+                return res.status(401).json({
+                    status: 401,
+                    error: 'Invalid',
+                    message: 'Token is invalid or empty.',
+                });
+            }
+            if (err) {
+                return next(err);
+            }
+            if (!user) {
+                return res.status(401).json({
+                    status: 401,
+                    error: 'Unauthorized',
+                    message: 'You are not authorized to access this resource.',
+                });
+            }
+            req.user = user;
+            next();
+        })(req, res, next); 
+    }
+
+    module.exports ={
+        authenticateLocal: passport.authenticate('local', { session: false }),
+        authenticateJWT: authenticateJWT,
+    }
