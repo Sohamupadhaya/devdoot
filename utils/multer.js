27ffb0e7b31492049e40cd7012e387df96bb814f
AshmitaChimo <asmita.chimoria@varosatech.com>
2025-05-02 14:54:34 +0545

    check if the directory exist if not create it

--- utils/multer.js
+++ utils/multer.js
@@ -1,19 +1,26 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads/profile')
+        const uploadPath = './uploads/profile';
+
+        if (!fs.existsSync(uploadPath)) {
+            fs.mkdirSync(uploadPath, { recursive: true });
+        }
+
+        cb(null, uploadPath);
     },
     filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname))
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
     }
-})
+});
 
 const uploadProfileImage = multer({
     storage: storage,
-    limits: { fileSize: 20*1024*1024 },
+    limits: { fileSize: 20 * 1024 * 1024 },
     fileFilter: function (req, file, cb) {
         const filetypes = /jpeg|jpg|png|gif/;
         const mimetype = filetypes.test(file.mimetype);
@@ -24,12 +31,12 @@ const uploadProfileImage = multer({
         } else {
             cb("Error: File upload only supports the following filetypes - " + filetypes);
         }
-    } 
-})
+    }
+});
 
-const uploadProfileImages= uploadProfileImage.fields([{
+const uploadProfileImages = uploadProfileImage.fields([{
     name: 'photo'
-}])
+}]);
 
 module.exports = {
     uploadProfileImages
