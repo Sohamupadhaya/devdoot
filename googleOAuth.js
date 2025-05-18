4672ba2eb89b8371c09026b44849be58257c9f66
ashmita246 <ashmitachimouriya123@gmail.com>
2025-05-18 16:10:55 +0545

    changes in the google Oauth file

--- googleOAuth.js
+++ googleOAuth.js
@@ -13,15 +13,9 @@ const oAuth2Client = new OAuth2Client(
 async function setRedirectURIs(req, res, next) {
   // Check request domain
   let redirectUri;
-  if (req.headers.host === "localhost:5000") {
-    redirectUri = "http://localhost:5000/login/google";
-  } else if (req.headers.origin === "http://localhost:5173") {
-    redirectUri = "http://localhost:5173/login/google";
-  } else if (req.headers.origin === "http://localhost:3000") {
+  if (req.headers.host === "localhost:3000") {
     redirectUri = "http://localhost:3000/login/google";
-  } else if (req.headers.origin === "https://zippyplan-yoddha.vercel.app/") {
-    redirectUri = "https://zippyplan-yoddha.vercel.app//login/google";
-  }
+  } 
   // Set the dynamic redirect URI in OAuth client options
   oAuth2Client.redirectUri = redirectUri;
   // Attach OAuth client to request object
